5b06b8488abd31d0dc80d063748208f63993a939
DanielePilati <danielepdj@gmail.com>
2024-05-05 20:11:04 +0200

    [upd] add bonus

--- js/main.js
+++ js/main.js
@@ -5,4 +5,17 @@ let userName = prompt('inserisci il tuo nome');
 let userSubName = prompt('inserisci il tuo cognome');
 let usercolor = prompt('inserisci il tuo colore preferito');
 
+
+let firstNumber = Number(prompt('inserisci il primo numero'));
+let secondNumber = Number(prompt('inserisci il secondo numero'));
+
+
 console.log(userName + userSubName + usercolor + 21);
+
+console.log(userName + userSubName + usercolor + (firstNumber/secondNumber));
+
+
+
+
+
+
