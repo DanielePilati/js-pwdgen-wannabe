1584a5d1d97027696fc2cea822ba5f237edd6d85
DanielePilati <danielepdj@gmail.com>
2024-05-05 22:13:06 +0200

    [upd] add new pass

--- js/main.js
+++ js/main.js
@@ -18,7 +18,9 @@ password = userName + userSubName + usercolor + (firstNumber / secondNumber);
 console.log(password);
 
 document.getElementById('new-password').innerHTML = password;
-document.getElementById('new-password-two').innerHTML = password = usercolor + userName + userSubName + (secondNumber/ firstNumber);
+document.getElementById('new-password-two').innerHTML = password = usercolor + userName + userSubName + (secondNumber / firstNumber);
+document.getElementById('new-password-three').innerHTML = password = userSubName + usercolor + userName + (secondNumber + firstNumber);
+
 
 
 
