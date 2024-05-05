b9ae35f1fd198083548c63db4c4d2ef45cd8d9b1
DanielePilati <danielepdj@gmail.com>
2024-05-05 19:59:42 +0200

    [upd] add script

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+let userName = prompt('inserisci il tuo nome');
+let userSubName = prompt('inserisci il tuo cognome');
+let usercolor = prompt('inserisci il tuo colore preferito');
+
+console.log(userName + userSubName + usercolor + 21);
