5ab5577eeb9a2f22c5a5c197a8b4faac3184548c
DanielePilati <danielepdj@gmail.com>
2024-05-05 21:07:19 +0200

    [upd] add page view

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 'use strict';
 
-
 let userName = prompt('inserisci il tuo nome');
 let userSubName = prompt('inserisci il tuo cognome');
 let usercolor = prompt('inserisci il tuo colore preferito');
-
-
 let firstNumber = Number(prompt('inserisci il primo numero'));
 let secondNumber = Number(prompt('inserisci il secondo numero'));
 
